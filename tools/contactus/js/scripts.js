d518df5baeac7fe6f2833eab15edd85748fbe72c
Shaik-Tajuddin <stajuddin@wearebarbarian.com>
2025-08-21 23:06:52 +0530

    custom sidekick form

---
+++ tools/contactus/js/scripts.js
@@ -0,0 +1,31 @@
+document.addEventListener("readystatechange", () => {
+  console.log("--Form has been loadded---");
+  if (document.readyState == "complete") {
+    const form = document.forms[0];
+    const submitButton = document.getElementById("submitButton");
+    console.log("====CLICKED in JS Form========{} : {}", form.id, submitButton.id);
+    const formSubmitHandler = (event) => {
+      event.preventDefault();
+      console.log("---Submitting form ----");
+      document.getElementById("name");
+      console.log(" NAME ->", document.getElementById("name").value);
+
+      const formdata = new FormData();
+      formdata.append("name", document.getElementById("name").value);
+      formdata.append("phone", document.getElementById("phone").value);
+      formdata.append("email", document.getElementById("email").value);
+      formdata.append("message", document.getElementById("message").value);
+      console.log("---Submitting form 2----");
+      const requestOptions = {
+        method: "POST", body: formdata, redirect: "follow"
+      };
+
+      fetch("https://docs.google.com/spreadsheets/d/1Iioe1_CHYwhk6P9ILWBviTLdx5uLpktXQybjkyuWZVc/edit?gid=0#gid=0", requestOptions)
+        .then((response) => response.text())
+        .then((result) => console.log(result))
+        .catch((error) => console.error(error));
+    };
+    //submitButton.addEventListener("click", formSubmitHandler);
+    form.addEventListener("submit", formSubmitHandler);
+  }
+});

0f06dc74371c2973b43be50aa3def85304d60ed7
Shaik-Tajuddin <stajuddin@wearebarbarian.com>
2025-08-26 20:53:00 +0530

    fix build pipeline failure

--- tools/contactus/js/scripts.js
+++ tools/contactus/js/scripts.js
@@ -1,31 +1,31 @@
-document.addEventListener("readystatechange", () => {
-  console.log("--Form has been loadded---");
-  if (document.readyState == "complete") {
+document.addEventListener('readystatechange', () => {
+  console.log('--Form has been loadded---');
+  if (document.readyState === 'complete') {
     const form = document.forms[0];
-    const submitButton = document.getElementById("submitButton");
-    console.log("====CLICKED in JS Form========{} : {}", form.id, submitButton.id);
+    const submitButton = document.getElementById('submitButton');
+    console.log('====CLICKED in JS Form========{} : {}', form.id, submitButton.id);
     const formSubmitHandler = (event) => {
       event.preventDefault();
-      console.log("---Submitting form ----");
-      document.getElementById("name");
-      console.log(" NAME ->", document.getElementById("name").value);
+      console.log('---Submitting form ----');
+      document.getElementById('name');
+      console.log(' NAME ->', document.getElementById('name').value);
 
       const formdata = new FormData();
-      formdata.append("name", document.getElementById("name").value);
-      formdata.append("phone", document.getElementById("phone").value);
-      formdata.append("email", document.getElementById("email").value);
-      formdata.append("message", document.getElementById("message").value);
-      console.log("---Submitting form 2----");
+      formdata.append('name', document.getElementById('name').value);
+      formdata.append('phone', document.getElementById('phone').value);
+      formdata.append('email', document.getElementById('email').value);
+      formdata.append('message', document.getElementById('message').value);
+      console.log('---Submitting form 2----');
       const requestOptions = {
-        method: "POST", body: formdata, redirect: "follow", mode: "no-cors"
+        method: 'POST', body: formdata, redirect: 'follow', mode: 'no-cors',
       };
 
-      fetch("https://webhook.site/a98cbc34-d8b6-4e1b-8f5d-e249098bcff3", requestOptions)
+      fetch('https://webhook.site/a98cbc34-d8b6-4e1b-8f5d-e249098bcff3', requestOptions)
         .then((response) => response.text())
         .then((result) => console.log(result))
         .catch((error) => console.error(error));
     };
-    //submitButton.addEventListener("click", formSubmitHandler);
-    form.addEventListener("submit", formSubmitHandler);
+    // submitButton.addEventListener("click", formSubmitHandler);
+    form.addEventListener('submit', formSubmitHandler);
   }
 });
